c1ba7ae734f29474356f5a3c043caaa92f459ac7
Evan Plaice <evanplaice@gmail.com>
2019-12-10 15:19:02 -0700

    Rework the Contact Section

--- src/sections/wc-contact.js
+++ src/sections/wc-contact.js
@@ -21,9 +21,11 @@ export class WCContact extends HTMLElement {
   async render () {
     const tags = {
       name: this.__data.name,
+      label: this.__data.label,
       email: this.__data.email,
       phone: this.__data.phone,
-      website: this.__data.url
+      website: this.__data.url,
+      location: this.__data.location
     };
     if (!this.__template) {
       this.__template = WCContact.default(tags);
@@ -31,12 +33,13 @@ export class WCContact extends HTMLElement {
     this.innerHTML = Interpolate(this.__template, tags);
   }
 
-  static default ({ name, email, phone, website }) {
+  static default ({ name, label, email, phone, website, location }) {
     return `
       <section id="contact">
-        <h1>${name}</h1>
+        <h1>${name}, ${label}</h1>
         <div class="email"><a href="mailto:${email}">${email}</a></div>
         <div class="phone">${phone}</div>
+        <div class="location">${location.city}, ${location.region}, ${location.countryCode}</div>
         <div class="website"><a href="${website}">${website}</a></div>
       </section>
       <hr>`;
